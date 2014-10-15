54b9cc835554b8b2a75b5db9bf14bc16b4025b5e
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-10-15 14:38:25 +0000

    update
    Former-commit-id: f39254a1404974976684679c7d89a9dd91ddd4e3

--- build_example_trajectory.bat
+++ build_example_trajectory.bat
@@ -3,6 +3,10 @@
 set MOT1=MTR0101
 set MOT2=MTR0102
 
+for /L %%i in (1,1,8) do (
+    caput %MYPVPREFIX%MOT:DMC01:Prof1:MTR010%%i:UseAxis 0
+)
+
 REM first axis positions, number of points must agree with NumPoints below
 REM caput -a -n %MYPVPREFIX%MOT:DMC01:Prof1:%MOT1%:Positions 5 0 0.8726535498 1.4 2.0 3.0 
 caput %MYPVPREFIX%MOT:DMC01:Prof1:%MOT1%:UseAxis 1
@@ -16,7 +20,7 @@ caput %MYPVPREFIX%MOT:DMC01:Prof1:%MOT2%:MoveMode Absolute
 REM profile properties
 caput %MYPVPREFIX%MOT:DMC01:Prof1:TimeMode Fixed
 caput %MYPVPREFIX%MOT:DMC01:Prof1:FixedTime 6
-caput %MYPVPREFIX%MOT:DMC01:Prof1:Acceleration 2 
+REM caput %MYPVPREFIX%MOT:DMC01:Prof1:Acceleration 2 
 REM caput %MYPVPREFIX%MOT:DMC01:Prof1:NumPoints 5
 
 REM generate profile and print out any error messages
