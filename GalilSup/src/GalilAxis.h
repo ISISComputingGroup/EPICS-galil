2c734c284df3873c6e1f3c61adb687ad0350fcdd
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-11-11 20:39:31 +0000

    Allow HOMING_TIMEOUT set by env variable for testing

--- GalilSup/src/GalilAxis.h
+++ GalilSup/src/GalilAxis.h
@@ -26,7 +26,7 @@
 
 #define KPMAX			1023.875
 #define KDMAX			4095.875
-#define HOMING_TIMEOUT		3.5
+#define HOMING_TIMEOUT	(getenv("HOMING_TIMEOUT") != NULL ? atof(getenv("HOMING_TIMEOUT")) : 3.5)
 
 //pollServices request numbers
 static const int MOTOR_STOP = 0;
